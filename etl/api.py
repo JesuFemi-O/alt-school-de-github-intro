611f762d1ce711c34008cd4cc604e4be5039dd4c
Emmanuel Ogunwede <47831972+JesuFemi-O@users.noreply.github.com>
2023-12-02 19:30:44 +0100

    updated shopify service

--- etl/api.py
+++ etl/api.py
@@ -2,5 +2,9 @@
 def fetch_shopify_orders():
     print("calling the shopify api to get orders placed via shopify")
 
+def fetch_order_tags(order_id):
+    print(f"fetching order tags for order_id {order_id}...")
+    return ['whatsapp', 'new-customer', 'spender']
+
 if __name__ == "__main__":
     fetch_shopify_orders()

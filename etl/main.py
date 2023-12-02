f7ffba42d6099eee83f422e1f8d86cb27c7efb82
Jesufemi-O <ogunwedeemmanuel@gmail.com>
2023-12-02 18:48:30 +0100

    add etl files

---
+++ etl/main.py
@@ -0,0 +1,11 @@
+"""
+This script serves as an entrypoint to our data pipeline
+for orchestrating the Alt school DE Project.
+
+"""
+
+def run():
+    print("Starting etl pipeline...")
+
+if __name__ == "__main__":
+    run()

2dac18ac21d17405660a11b18f5ac45a3a22a85d
Jesufemi-O <ogunwedeemmanuel@gmail.com>
2023-12-02 18:59:55 +0100

    updated etl pipeline

--- etl/main.py
+++ etl/main.py
@@ -4,8 +4,17 @@ for orchestrating the Alt school DE Project.
 
 """
 
+def run_raw_pipeline():
+    print("Starting etl on raw daatabase pipeline...")
+
+def run_services_pipeline():
+    print("Starting etl on services database pipeline...")
+
 def run():
-    print("Starting etl pipeline...")
+    print("starting etl pipeline..")
+    run_raw_pipeline()
+    run_services_pipeline()
+
 
 if __name__ == "__main__":
     run()

9c4d76057c28c7c445c774d4e542939a4905fef3
Jesufemi-O <ogunwedeemmanuel@gmail.com>
2023-12-02 19:50:29 +0100

    created streamlit data application

---
+++ data-apps/main.py
@@ -0,0 +1,19 @@
+"""
+This script represents a streamlit application designed to give
+c-level execs near real time updates on some important metrics in
+our organistion like:
+
+- revenue
+- number of active customers
+- total sales made on current day
+- cancelled sales on current day
+- active vehicles
+"""
+
+
+def start_streamlit():
+    print("starting streamlit app...")
+
+
+if __name__ == "__main__":
+    start_streamlit()
